134010a4ec12736c65428175948747b4ff3d082e
Bennett Clement <benclmnt.dev@gmail.com>
2021-01-08 16:48:46 +0800

    Add game state

---
+++ gameState.js
@@ -0,0 +1,13 @@
+export const initGameState = {
+    level: 1,
+    distance: 0,
+    speed: 5,
+    gameStatus: "play",
+    floorRotation: 0,
+    monsterPos: .65,
+    monsterPosTarget: .65
+}
+
+export const gameLimit = {
+    maxSpeed: 48
+}
