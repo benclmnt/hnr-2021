4e56b98671164a9debe317bda7444d94abbdcfce
Louis <louis@Louiss-MacBook-Air.local>
2021-01-08 18:48:52 +0800

    Add firs

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
-import * as Material from './materials.js';
 import { shadowLight } from './lights.js';
 import floor from './floor.js';
 import Hero from './hero.js';
 import { initGameState, gameLimit } from './gameState.js';
+import trunc from './trunc.js';
+import fir from './fir.js';
 
-let scene, camera, clock, renderer, backLight;
+let scene, camera, clock, renderer;
 
 let delta = 0;
+let floorRadius = 200;
 let monsterAcceleration = 0.004;
 let malusClearColor = 0xb44b39;
 let malusClearAlpha = 0;
-let audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/264161/Antonio-Vivaldi-Summer_01.mp3');
 
 // scene background
 let sceneBackgroundColor = 0xAAAAAA;
@@ -21,24 +22,17 @@ const nearPlane = 1;
 const farPlane = 2000;
 const fov = 50; // field of view
 let cameraPosGame = 160;
-let cameraPosGameOver = 260;
 
 // characters
-let hero, monster;
+let hero;
 
 let fieldGameOver, fieldDistance;
 
 // game status
 let {
     speed,
-    distance,
-    level,
-    gameStatus,
-    floorRotation,
-    monsterPos,
-    monsterPosTarget } = initGameState;
+    gameStatus } = initGameState;
 let { maxSpeed } = gameLimit;
-let levelInterval;
 let levelUpdateFreq = 3000; // 3s
 
 //INIT THREE JS, SCREEN AND MOUSE EVENTS
@@ -169,23 +163,42 @@ function resetGameDefault() {
     hero.mesh.rotation.y = Math.PI / 2;
 
     speed = initGameState.speed;
-    level = initGameState.level - 1;
-    distance = initGameState.distance;
     gameStatus = initGameState.gameStatus;
 
-    monsterPos = initGameState.monsterPos;
-    monsterPosTarget = initGameState.monsterPosTarget;
 
     hero.status = "running";
     hero.nod();
 
     // audio.play();
     updateLevel();
-    levelInterval = setInterval(updateLevel, levelUpdateFreq);
 
 }
 
 // listeners utilities
+
+
+var firs = new THREE.Group();
+
+function createFirs() {
+
+    var nTrees = 100;
+    for (var i = 0; i < nTrees; i++) {
+        var phi = i * (Math.PI * 2) / nTrees;
+        var theta = Math.PI / 2;
+        //theta += .25 + Math.random()*.3; 
+        theta += (Math.random() > .05) ? .25 + Math.random() * .3 : - .35 - Math.random() * .1;
+
+        var fir = new tree();
+        fir.mesh.position.x = Math.sin(theta) * Math.cos(phi) * floorRadius;
+        fir.mesh.position.y = Math.sin(theta) * Math.sin(phi) * (floorRadius - 10);
+        fir.mesh.position.z = Math.cos(theta) * floorRadius;
+
+        var vec = fir.mesh.position.clone();
+        var axis = new THREE.Vector3(0, 1, 0);
+        fir.mesh.quaternion.setFromUnitVectors(axis, vec.clone().normalize());
+        floor.add(fir.mesh);
+    }
+}
 
 function handleWindowResize() {
     const HEIGHT = window.innerHeight;
@@ -197,15 +210,24 @@ function handleWindowResize() {
 
 // main function
 
-function init(event) {
+function init() {
     initScreenAnd3D();
     initUI();
     createLights();
     createFloor();
     createHero();
+    createFirs();
     initUI();
     resetGameDefault();
     loop();
+}
+
+class tree {
+    constructor() {
+        this.mesh = new THREE.Object3D();
+        this.trunc = new trunc();
+        this.mesh.add(this.trunc.mesh);
+    }
 }
 
 init();
