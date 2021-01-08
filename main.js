840cc23a3c9d8f7ea485f11ddd8899f9ceb17d0c
Kevin William <kevinwilliam1907@gmail.com>
2021-01-08 16:51:05 +0800

    Add game menu

--- main.js
+++ main.js
@@ -63,6 +63,50 @@ function initScreenAnd3D() {
 
     // event listeners
     window.addEventListener('resize', handleWindowResize, false);
+    document.addEventListener("keydown", function(event) {
+        if (event.key === 'Escape'){
+           // Esc key was pressed
+           handleEscape();
+       }
+    });
+}
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal')
+      closeModal(modal)
+      handleEscape()
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+  
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+function handleEscape() {
+    if (gameStatus == "paused") {
+        const modals = document.querySelectorAll('.modal.active');
+        modals.forEach(modal => {
+            closeModal(modal)
+        })
+        gameStatus = "play"
+        clock.start()
+        loop()
+    } else if (gameStatus == "play") {
+        const modal = document.querySelector('#modal')
+        gameStatus = "paused"
+        openModal(modal)
+    }
 }
 
 function createLights() {
